9ee3629402f679a3b81834ef938ed56ebd5175c6
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2018-10-29 22:15:59 -0400

    Set dll output path.

--- cmake/mmg.cmake
+++ cmake/mmg.cmake
@@ -209,7 +209,11 @@ target_include_directories(mmg PUBLIC SYSTEM
     ${MMG_BINARY_DIR}
 )
 
-set_target_properties(${target_name} PROPERTIES CXX_VISIBILITY_PRESET hidden)
+set_target_properties(mmg PROPERTIES CXX_VISIBILITY_PRESET hidden)
+
+set_target_properties(mmg PROPERTIES LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}")
+set_target_properties(mmg PROPERTIES ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}")
+set_target_properties(mmg PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}")
 
 ################################################################################
 
